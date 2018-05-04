54f03a745539c2e155f4f425f3fcae8effdb39f3
RED-GATE\Brian.McRae <brian.mcrae@red-gate.com>
2018-05-04 08:55:59 -0500

    Contact Type Changes

---
+++ Ally_Demo/Migrations/1.1.0-Changes/001_20180504-0853_Brian.McRae.sql
@@ -0,0 +1,14 @@
+﻿-- <Migration ID="412227f3-145e-4baa-b6da-fbf1d77b1271" />
+GO
+
+PRINT N'Disabling DDL triggers'
+GO
+DISABLE TRIGGER ALL ON DATABASE
+GO
+PRINT N'Altering [Person].[ContactType]'
+GO
+ALTER TABLE [Person].[ContactType] ADD
+[Comments] [varchar] (100) NULL
+GO
+PRINT N'Re-enabling DDL triggers'
+GO
