f0667245eedf6a1e76f3d9ba9ac1814f76b9bd38
RED-GATE\Brian.McRae <brian.mcrae@red-gate.com>
2018-05-07 10:17:52 -0500

    Contact Type Changes

---
+++ Ally_Demo/Migrations/1.1.0-Changes/002_20180504-1500_Brian.McRae.sql
@@ -0,0 +1,19 @@
+﻿-- <Migration ID="2a013132-289b-4361-bb40-f7aa31c0277a" />
+GO
+
+PRINT N'Disabling DDL triggers'
+GO
+DISABLE TRIGGER ALL ON DATABASE
+GO
+PRINT N'Dropping [Person].[GetContactTypeData]'
+GO
+IF OBJECT_ID(N'[Person].[GetContactTypeData]', 'P') IS NOT NULL
+DROP PROCEDURE [Person].[GetContactTypeData]
+GO
+PRINT N'Altering [Person].[ContactType]'
+GO
+IF COL_LENGTH(N'[Person].[ContactType]', N'Comments') IS NOT NULL
+ALTER TABLE [Person].[ContactType] DROP COLUMN [Comments]
+GO
+PRINT N'Re-enabling DDL triggers'
+GO
---
+++ Ally_Demo/Migrations/1.1.0-Changes/003_20180507-0923_Brian.McRae.sql
@@ -0,0 +1,14 @@
+﻿-- <Migration ID="86cfdcfb-8ade-41b5-af0b-e66a0fa182ee" />
+GO
+
+PRINT N'Disabling DDL triggers'
+GO
+DISABLE TRIGGER ALL ON DATABASE
+GO
+PRINT N'Altering [Person].[ContactType]'
+GO
+ALTER TABLE [Person].[ContactType] ADD
+[Comments] [varchar] (100) NULL
+GO
+PRINT N'Re-enabling DDL triggers'
+GO
