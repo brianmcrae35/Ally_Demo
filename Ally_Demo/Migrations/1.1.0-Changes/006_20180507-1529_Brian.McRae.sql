c46fe31eb982070b3d596643b393e53f0232288f
RED-GATE\Brian.McRae <brian.mcrae@red-gate.com>
2018-05-07 15:30:04 -0500

    Added TestData Table

---
+++ Ally_Demo/Migrations/1.1.0-Changes/006_20180507-1529_Brian.McRae.sql
@@ -0,0 +1,22 @@
+﻿-- <Migration ID="65642ce2-05b1-46d2-9591-71c08a3a191f" />
+GO
+
+PRINT N'Disabling DDL triggers'
+GO
+DISABLE TRIGGER ALL ON DATABASE
+GO
+PRINT N'Creating [dbo].[TestData]'
+GO
+CREATE TABLE [dbo].[TestData]
+(
+[FirstName] [nvarchar] (50) NULL,
+[LastName] [nvarchar] (50) NULL,
+[PhoneNumber] [nvarchar] (50) NULL,
+[AddressLine1] [nvarchar] (200) NULL,
+[City] [nvarchar] (50) NULL,
+[State] [nvarchar] (50) NULL,
+[FullName] [varchar] (50) NULL
+)
+GO
+PRINT N'Re-enabling DDL triggers'
+GO
---
+++ Ally_Demo/Schema-Model/dbo/Tables/TestData.sql
@@ -0,0 +1,11 @@
+CREATE TABLE [dbo].[TestData]
+(
+[FirstName] [nvarchar] (50) NULL,
+[LastName] [nvarchar] (50) NULL,
+[PhoneNumber] [nvarchar] (50) NULL,
+[AddressLine1] [nvarchar] (200) NULL,
+[City] [nvarchar] (50) NULL,
+[State] [nvarchar] (50) NULL,
+[FullName] [varchar] (50) NULL
+)
+GO
