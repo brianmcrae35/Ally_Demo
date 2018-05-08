544888d3c03e11c6e89b1612d68b231209061a07
RED-GATE\Brian.McRae <brian.mcrae@red-gate.com>
2018-05-08 10:42:23 -0500

    Test Demo Changes

---
+++ Ally_Demo/Migrations/1.1.0-Changes/010_20180508-1041_Brian.McRae.sql
@@ -0,0 +1,24 @@
+﻿-- <Migration ID="90aea3b1-0b87-453c-9be0-6d147269c7dd" />
+GO
+
+PRINT N'Disabling DDL triggers'
+GO
+DISABLE TRIGGER ALL ON DATABASE
+GO
+PRINT N'Dropping [dbo].[SP_GetContactType]'
+GO
+IF OBJECT_ID(N'[dbo].[SP_GetContactType]', 'P') IS NOT NULL
+DROP PROCEDURE [dbo].[SP_GetContactType]
+GO
+PRINT N'Altering [Person].[AddressType]'
+GO
+IF COL_LENGTH(N'[Person].[AddressType]', N'Comments') IS NULL
+ALTER TABLE [Person].[AddressType] ADD[Comments] [varchar] (100) NULL
+GO
+PRINT N'Altering [Person].[ContactType]'
+GO
+IF COL_LENGTH(N'[Person].[ContactType]', N'Comments') IS NULL
+ALTER TABLE [Person].[ContactType] ADD[Comments] [varchar] (100) NULL
+GO
+PRINT N'Re-enabling DDL triggers'
+GO
---
+++ Ally_Demo/Programmable Objects/Person/Stored Procedures/GetAddressTypeData.sql
@@ -0,0 +1,15 @@
+﻿IF OBJECT_ID('[Person].[GetAddressTypeData]') IS NOT NULL
+	DROP PROCEDURE [Person].[GetAddressTypeData];
+
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+
+CREATE PROCEDURE [Person].[GetAddressTypeData] /* Violate ST003 no BEGIN...END in procedure */
+AS
+/*BEGIN*/
+SELECT * FROM Person.AddressType /* Violate BP005 - Asterisk in SELECT list */
+/*END*/
+GO
