bdffdfa11673c08c52abe7306c7c81dc4b503d8d
RED-GATE\Brian.McRae <brian.mcrae@red-gate.com>
2018-05-08 08:14:52 -0500

    Renamed SP to fail SQL Test

---
+++ Ally_Demo/Migrations/1.1.0-Changes/007_20180508-0814_Brian.McRae.sql
@@ -0,0 +1,14 @@
+﻿-- <Migration ID="d4663d80-0843-430d-89af-52d27227bb0a" />
+GO
+
+PRINT N'Disabling DDL triggers'
+GO
+DISABLE TRIGGER ALL ON DATABASE
+GO
+PRINT N'Dropping [dbo].[GetContactType]'
+GO
+IF OBJECT_ID(N'[dbo].[GetContactType]', 'P') IS NOT NULL
+DROP PROCEDURE [dbo].[GetContactType]
+GO
+PRINT N'Re-enabling DDL triggers'
+GO
