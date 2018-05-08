c96fafa3573b16eb42509ca45f1de0c88c57e3ef
RED-GATE\Brian.McRae <brian.mcrae@red-gate.com>
2018-05-08 13:09:23 -0500

    database changes

---
+++ Ally_Demo/Migrations/1.1.0-Changes/011_20180508-1045_Brian.McRae.sql
@@ -0,0 +1,34 @@
+﻿-- <Migration ID="4e3c2317-decf-4189-9172-3cddb3db6a4d" />
+GO
+
+PRINT N'Disabling DDL triggers'
+GO
+DISABLE TRIGGER ALL ON DATABASE
+GO
+PRINT N'Dropping [Person].[GetContactTypeData]'
+GO
+IF OBJECT_ID(N'[Person].[GetContactTypeData]', 'P') IS NOT NULL
+DROP PROCEDURE [Person].[GetContactTypeData]
+GO
+PRINT N'Dropping [Person].[GetAddressTypeData]'
+GO
+IF OBJECT_ID(N'[Person].[GetAddressTypeData]', 'P') IS NOT NULL
+DROP PROCEDURE [Person].[GetAddressTypeData]
+GO
+PRINT N'Dropping [dbo].[uspGetContactType]'
+GO
+IF OBJECT_ID(N'[dbo].[uspGetContactType]', 'P') IS NOT NULL
+DROP PROCEDURE [dbo].[uspGetContactType]
+GO
+PRINT N'Altering [Person].[AddressType]'
+GO
+IF COL_LENGTH(N'[Person].[AddressType]', N'Comments') IS NOT NULL
+ALTER TABLE [Person].[AddressType] DROP COLUMN [Comments]
+GO
+PRINT N'Altering [Person].[ContactType]'
+GO
+IF COL_LENGTH(N'[Person].[ContactType]', N'Comments') IS NOT NULL
+ALTER TABLE [Person].[ContactType] DROP COLUMN [Comments]
+GO
+PRINT N'Re-enabling DDL triggers'
+GO
--- Ally_Demo/Programmable Objects/dbo/Stored Procedures/uspGetContactType.sql
+++ Ally_Demo/Programmable Objects/dbo/Stored Procedures/uspGetContactType.sql
@@ -1,17 +1,4 @@
-﻿IF OBJECT_ID('[dbo].[uspGetContactType]') IS NOT NULL
+IF OBJECT_ID('[dbo].[uspGetContactType]') IS NOT NULL
 	DROP PROCEDURE [dbo].[uspGetContactType];
 
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-SET ANSI_NULLS ON
-GO
---SET QUOTED_IDENTIFIER ON|OFF
---SET ANSI_NULLS ON|OFF
---GO
-CREATE PROCEDURE [dbo].[uspGetContactType]
-    @parameter_name AS INT
--- WITH ENCRYPTION, RECOMPILE, EXECUTE AS CALLER|SELF|OWNER| 'user_name'
-AS
-    SELECT * FROM Person.ContactType
 GO
