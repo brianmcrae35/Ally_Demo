cf780e3f69b521e9cd6383ed42f151648abde68d
RED-GATE\Brian.McRae <brian.mcrae@red-gate.com>
2018-05-09 08:56:46 -0500

    Rename USP

---
+++ Ally_Demo/Migrations/1.1.0-Changes/020_20180509-0856_Brian.McRae.sql
@@ -0,0 +1,14 @@
+﻿-- <Migration ID="18878024-58ac-4696-9a3c-71d5a4bb53bd" />
+GO
+
+PRINT N'Disabling DDL triggers'
+GO
+DISABLE TRIGGER ALL ON DATABASE
+GO
+PRINT N'Dropping [dbo].[uspGetContactType]'
+GO
+IF OBJECT_ID(N'[dbo].[uspGetContactType]', 'P') IS NOT NULL
+DROP PROCEDURE [dbo].[uspGetContactType]
+GO
+PRINT N'Re-enabling DDL triggers'
+GO
