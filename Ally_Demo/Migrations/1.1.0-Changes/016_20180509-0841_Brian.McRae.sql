310984dbdaa892983fcc67e60ff8fc2d1a0a0aed
RED-GATE\Brian.McRae <brian.mcrae@red-gate.com>
2018-05-09 08:42:25 -0500

    Test DB Changes

---
+++ Ally_Demo/Migrations/1.1.0-Changes/016_20180509-0841_Brian.McRae.sql
@@ -0,0 +1,19 @@
+﻿-- <Migration ID="349664d8-db4f-444b-adee-9aa93df98382" />
+GO
+
+PRINT N'Disabling DDL triggers'
+GO
+DISABLE TRIGGER ALL ON DATABASE
+GO
+PRINT N'Altering [Person].[AddressType]'
+GO
+ALTER TABLE [Person].[AddressType] ADD
+[Comments] [varchar] (100) NULL
+GO
+PRINT N'Altering [Person].[ContactType]'
+GO
+ALTER TABLE [Person].[ContactType] ADD
+[Comments] [varchar] (100) NULL
+GO
+PRINT N'Re-enabling DDL triggers'
+GO
---
+++ Ally_Demo/Migrations/1.1.0-Changes/017_20180509-0841_Brian.McRae.sql
@@ -0,0 +1,30 @@
+﻿-- <Migration ID="fb40c409-495b-46f4-be04-f2c610958614" />
+GO
+
+
+SET DATEFORMAT YMD;
+
+
+GO
+PRINT (N'Add 1 row to [Person].[AddressType]');
+
+SET IDENTITY_INSERT [Person].[AddressType] ON;
+
+INSERT  INTO [Person].[AddressType] ([AddressTypeID], [Name], [rowguid], [ModifiedDate], [Comments])
+VALUES                             (10, N'Regional Office', 'be4cfa0f-284a-44c4-b1a4-7bf3658e8f78', '2018-05-09 08:39:29.517', NULL);
+
+SET IDENTITY_INSERT [Person].[AddressType] OFF;
+
+
+GO
+PRINT (N'Add 1 row to [Person].[ContactType]');
+
+SET IDENTITY_INSERT [Person].[ContactType] ON;
+
+INSERT  INTO [Person].[ContactType] ([ContactTypeID], [Name], [ModifiedDate], [Comments])
+VALUES                             (28, N'Regional Office', '2018-05-09 08:39:29.537', NULL);
+
+SET IDENTITY_INSERT [Person].[ContactType] OFF;
+
+
+GO
